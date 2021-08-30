ea000c750b2f975a329ed7be1448735417363105
horrible little slime <69secret69email69@gmail.com>
2021-08-29 23:22:44 -0400

    update linting stuff; expand outfit stuff

--- src/outfit.ts
+++ src/outfit.ts
@@ -1,75 +1,79 @@
-import { buy, effectModifier, mallPrice, myFamiliar, numericModifier } from "kolmafia";
-import { $effect, $familiar, $familiars, $item, $items, have, MaximizeOptions } from "libram";
+import {
+  buy,
+  effectModifier,
+  haveEquipped,
+  mallPrice,
+  myFamiliar,
+  numericModifier,
+} from "kolmafia";
+import {
+  $effect,
+  $familiar,
+  $familiars,
+  $item,
+  $items,
+  getFoldGroup,
+  have,
+  MaximizeOptions,
+} from "libram";
 import { clamp } from "./lib";
 
-export class Requirement {
-  maximizeParameters_: string[];
-  maximizeOptions_: MaximizeOptions;
-
-  constructor(maximizeParameters_: string[], maximizeOptions_: MaximizeOptions) {
-    this.maximizeParameters_ = maximizeParameters_;
-    this.maximizeOptions_ = maximizeOptions_;
-  }
-
-  maximizeParameters(): string[] {
-    return this.maximizeParameters_;
-  }
-
-  maximizeOptions(): MaximizeOptions {
-    return this.maximizeOptions_;
-  }
-
-  merge(other: Requirement): Requirement {
-    const optionsA = this.maximizeOptions();
-    const optionsB = other.maximizeOptions();
-    return new Requirement([...this.maximizeParameters(), ...other.maximizeParameters()], {
-      ...optionsA,
-      ...optionsB,
-      bonusEquip: new Map([
-        ...(optionsA.bonusEquip?.entries() ?? []),
-        ...(optionsB.bonusEquip?.entries() ?? []),
-      ]),
-      forceEquip: [...(optionsA.forceEquip ?? []), ...(optionsB.forceEquip ?? [])],
-      preventEquip: [...(optionsA.preventEquip ?? []), ...(optionsB.preventEquip ?? [])],
-      onlySlot: [...(optionsA.onlySlot ?? []), ...(optionsB.onlySlot ?? [])],
-      preventSlot: [...(optionsA.preventSlot ?? []), ...(optionsB.preventSlot ?? [])],
-    });
-  }
-
-  static merge(allRequirements: Requirement[]): Requirement {
-    return allRequirements.reduce((x, y) => x.merge(y), new Requirement([], {}));
-  }
-}
-
 const actionRateBonus = () =>
-  clamp(
-    numericModifier("Familiar Action Bonus") +
-      ($items`short stack of pancakes, short stick of butter, short glass of water`
-        .map((item) => effectModifier(item, "Effect"))
-        .some((effect) => have(effect))
-        ? 1
-        : 0),
-    0,
-    1
-  );
+  numericModifier("Familiar Action Bonus") / 100 +
+  ($items`short stack of pancakes, short stick of butter, short glass of water`
+    .map((item) => effectModifier(item, "Effect"))
+    .some((effect) => have(effect))
+    ? 1
+    : 0);
+
+type stasisValue = {
+  baseRate: number;
+  meatPerLb: number;
+};
 
 const trickHats = $items`invisible bag, witch hat, beholed bedsheet`;
 const adventureFamiliars = $familiars`temporal riftlet, unagnimated gnome`;
-const stasisFamiliars = new Map<Familiar, [Number, Number]>([
-  [$familiar`ninja pirate zombie robot`, [1 / 2, 0.11 * 6 * 11]],
-  [$familiar`Cocoabo`, [1 / 3, 1/4 * 1.2 * 4 * 11]],
-  [$familiar`stocking mimic`, [1 / 3, 1/4 * 1.2 * 4 * 11]],
-  [$familiar`feather boa constrictor`, [1 / 2, 0.5 * 5 * 11]],
+const stasisFamiliars = new Map<Familiar, stasisValue>([
+  [$familiar`ninja pirate zombie robot`, { baseRate: 1 / 2, meatPerLb: 14.52 }],
+  [$familiar`Cocoabo`, { baseRate: 1 / 3, meatPerLb: 13.2 }],
+  [$familiar`stocking mimic`, { baseRate: 1 / 3, meatPerLb: 13.2 }],
+  [$familiar`feather boa constrictor`, { baseRate: 1 / 3, meatPerLb: 27.5 }],
 ]);
 
 export function trickOutfit() {
   if (!trickHats.some((hat) => have(hat))) {
     buy(1, trickHats.sort((a, b) => mallPrice(b) - mallPrice(a))[0]);
   }
-
   const trickHat = trickHats.find((hat) => have(hat)) || $item`beholed bedsheet`; //Just to stop it from being undefined
-  const forceEquip = [trickHat];
+
+  const forceEquips = [trickHat];
+
+  const stasisData = stasisFamiliars.get(myFamiliar());
+  if (stasisData) {
+    if (
+      stasisData.baseRate + actionRateBonus() < 1 &&
+      getFoldGroup($item`Loathing Legion Helicopter`).some((foldable) => have(foldable))
+    ) {
+      forceEquips.push($item`loathing Legion helicopter`);
+    }
+  }
+
+  const baseMeat = 3;
+
+  const weightValue = stasisData
+    ? clamp(
+        stasisData.baseRate +
+          actionRateBonus() +
+          (forceEquips.includes($item`loathing legion helicopter`) &&
+          !haveEquipped($item`loathing legion helicopter`)
+            ? 0.25
+            : 0),
+        0,
+        1
+      ) * stasisData.meatPerLb
+    : adventureFamiliars.includes(myFamiliar())
+    ? (1000 * baseMeat) / Math.pow(1000 - 130, 2)
+    : 0;
 
   //Bonus Equips
-  //Value +weight
 }
