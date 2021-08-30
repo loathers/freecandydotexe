e24cca3c4f58bc2505838e05726b5bd2f11a49b4
horrible little slime <69secret69email69@gmail.com>
2021-08-30 03:10:48 -0400

    linting

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,3 +1,3 @@
-declare module 'canadv.ash' {
-    export function canAdv(location: Location, x: boolean): boolean;
+declare module "canadv.ash" {
+  export function canAdv(location: Location, x: boolean): boolean;
 }
