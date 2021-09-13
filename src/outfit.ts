08d477bdb33c8109b261f65a10b8849d88ed53a8
Samuel Gaus <sam@gaus.co.uk>
2021-09-13 11:17:04 +0100

    Maximizing requirements fails when you're sober

--- src/outfit.ts
+++ src/outfit.ts
@@ -412,7 +412,7 @@ export function meatOutfit(): void {
       ...(bjornalike ? new Map([[bjornalike, bjornValue(bjornFam)]]) : []),
     ]),
     preventEquip: $items`Buddy Bjorn, Crown of Thrones`.filter((bjorn) => bjorn !== bjornalike),
-    forceEquip: myInebriety() > inebrietyLimit() ? $items`Drunkula's wineglass` : undefined,
+    forceEquip: myInebriety() > inebrietyLimit() ? $items`Drunkula's wineglass` : [],
   }).maximize();
   if (haveEquipped($item`Buddy Bjorn`)) bjornifyFamiliar(bjornFam.familiar);
   else if (haveEquipped($item`Crown of Thrones`)) enthroneFamiliar(bjornFam.familiar);
