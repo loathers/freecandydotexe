73a254f53fb3778f49ab5592dc0f0863cc34d24d
horrible little slime <69secret69email69@gmail.com>
2023-07-17 14:24:52 -0400

    Re-fix it

--- src/trickTreatTasks.ts
+++ src/trickTreatTasks.ts
@@ -78,9 +78,9 @@ const TRICK_TREAT_TASKS: CandyTask[] = [
           } while (inMultiFight());
           return;
         }
-        if (tricked.length < HOUSE_NUMBERS.length) {
-          abort("We thought there were unvisited trickable houses left, but alas! there are not!");
-        }
+      }
+      if (tricked.length < HOUSE_NUMBERS.length) {
+        abort("We thought there were unvisited trickable houses left, but alas! there are not!");
       }
     },
     outfit: trickOutfit,
