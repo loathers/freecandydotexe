b4dd187f409b7d7a5ac0774ada073945cec1de8f
horrible little slime <69secret69email69@gmail.com>
2023-10-17 21:20:38 -0400

    account for blocks in estimatedTurns

--- src/wanderer.ts
+++ src/wanderer.ts
@@ -2,12 +2,13 @@ import { WandererManager } from "garbo-lib";
 import { getMonsters, myAdventures } from "kolmafia";
 import { $location } from "libram";
 import { freecandyValue } from "./value";
+import args from "./args";
 
 let _wanderer: WandererManager | null = null;
 export function wanderer(): WandererManager {
   return (_wanderer ??= new WandererManager({
     ascend: true,
-    estimatedTurns: myAdventures,
+    estimatedTurns: () => Math.min(myAdventures(), 5 * args.blocks),
     itemValue: freecandyValue,
     effectValue: () => 0,
     prioritizeCappingGuzzlr: false,
