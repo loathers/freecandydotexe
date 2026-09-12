9e1adf13f8d1a1ef0e26f1f00c17517bc8ec1330
libraryaddict <libraryaddict@users.noreply.github.com>
2026-09-12 13:48:42 +1200

    lint

--- src/outfit.ts
+++ src/outfit.ts
@@ -92,8 +92,8 @@ function ensureBjorn(weightValue: number, meatValue = 0): CrownOfThrones.Familia
 
 export function getTreatOutfit(): string {
   if (!args.treatOutfit) {
-    const availableOutfits = getOutfits().filter((name) =>
-      outfitTattoo(name) && outfitPieces(name).every((piece) => canEquip(piece))
+    const availableOutfits = getOutfits().filter(
+      (name) => outfitTattoo(name) && outfitPieces(name).every((piece) => canEquip(piece))
     );
 
     printError(`No treatOutfit given--doing some math to decide what to use`);
